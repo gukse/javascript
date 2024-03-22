a694a6568384a92c620e0611f1f339ea2f48e02e
Gustavo Krause <gugukrause@gmail.com>
2024-03-21 21:21:33 -0300

    aula 01

---
+++ Aula 01/index.js
@@ -0,0 +1,21 @@
+
+var Nome = "Gustavo"
+var Sobrenome = "Krause"
+var anoAtual = 2024
+var idade = 27
+
+var nomeCompleto = Nome + " " + Sobrenome
+var anoDeNascimento = anoAtual - idade
+
+console.log(nomeCompleto)
+console.log(anoDeNascimento)
+
+var numeroInserido1 = prompt("insira um numero")
+var numeroInserido2 = prompt("insira um segundo numero")
+
+var somaNumerosInseridos = parseInt(numeroInserido1) + parseInt(numeroInserido2)
+var produtoNumerosInseridos = numeroInserido1 * numeroInserido2
+
+console.log(somaNumerosInseridos)
+
+alert("o produto dos numeros é: " + produtoNumerosInseridos)
