f2a1178eb93b34c7999a28c314ad0e2ca2312929
Gustavo Krause <gugukrause@gmail.com>
2024-03-26 20:16:59 -0300

    primeira pratica da aula 2

---
+++ Aula 02/index.js
@@ -0,0 +1,20 @@
+
+var numeroInserido = prompt("insira um numero entre 1 e 5")
+if (numeroInserido == 1) {
+    alert("Numero um")
+}
+else if (numeroInserido == 2) {
+    alert("Numero dois")
+}
+else if (numeroInserido == 3) {
+    alert("Numero tres")
+}
+else if (numeroInserido == 4) {
+    alert("Numero quatro")
+}
+else if (numeroInserido == 5) {
+    alert("Numero cinco")
+}
+else {
+    alert ("Não é um numero entre 1 e 5")
+}
