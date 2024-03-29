f0bd46386d21937ef04e31c08a8646d057c42976
Gustavo Krause <gugukrause@gmail.com>
2024-03-28 22:11:58 -0300

    desafio aula 02

---
+++ Aula 02/Desafio Aula 2/index.js
@@ -0,0 +1,15 @@
+
+var nome = prompt("Digite seu nome:");
+alert("Olá, " + nome + "!");
+
+
+var numero1 = parseInt(prompt("Digite um número:"));
+var numero2 = 3; 
+var resultadoSoma = numero1 + numero2;
+console.log("A soma de " + numero1 + " e " + numero2 + " é: " + resultadoSoma);
+
+// Solicitar um texto pelo prompt, depois outro, concatená-los e mostrar o resultado através de alert
+var texto1 = prompt("Digite um texto:");
+var texto2 = prompt("Digite outro texto:");
+var resultadoConcatenacao = texto1 + " " + texto2;
+console.log("Juntando os textos: " + resultadoConcatenacao);
